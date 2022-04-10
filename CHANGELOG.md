fa6d8964aa7b94802dacb2396a38f4dd47aa10ac
diego sanchez <drasius@gmail.com>
2022-04-10 11:22:58 +0200

    EnumLanguage support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
+## 2.1.0
+- EnumLanguage support through EnumCountryName
 ## 2.0.0
-- Change to enum based that start with Enum prefix
+- Change to enum based. Classes starts with Enum prefix
 - All static method are in EnumCountryCodeExt extension
 
 ## 1.0.0
--- lib/iso_3166_1.dart
+++ lib/iso_3166_1.dart
@@ -1,9 +1,12 @@
 library iso_3166_1;
 
-export 'src/enum_country_code.dart';
 export 'src/enum_country_alpha2.dart';
 export 'src/enum_country_alpha3.dart';
+export 'src/enum_country_code.dart';
 export 'src/enum_country_name.dart';
-export 'src/country_code.dart';
+
+export 'src/enum_language.dart';
+
 export 'src/display_countries.dart';
+export 'src/country_code.dart';
 
